95ed777b614cd5788d8cdebb34ecab7ffea5e805
aliensmart <kaoua17@gmail.com>
2020-05-03 02:12:36 -0400

    login register and logout path added to the project urls.py

--- new_project/new_project/urls.py
+++ new_project/new_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
 ]
