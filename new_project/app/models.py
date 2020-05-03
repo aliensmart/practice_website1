4365483358cd20504f070ce0a3f612087bbd4134
aliensmart <kaoua17@gmail.com>
2020-05-03 00:50:43 -0400

    user profile created in the models.py

--- new_project/app/models.py
+++ new_project/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)#user will inherit everything from the User class (username, firstname, password, email, lastname)
+
+    #Additional data to add
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pictures', blank=True)
+
+
+    def __str__(self):
+        return self.user.name
+
+
+
