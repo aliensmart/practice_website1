2d5a9ccc0dcb711009ec31c5977c269dc0e56115
aliensmart <kaoua17@gmail.com>
2020-05-03 01:51:53 -0400

    all login register and logout function finished in views.py

--- new_project/app/views.py
+++ new_project/app/views.py
@@ -11,7 +11,7 @@ def index(request):
     return render(request, 'app/index.html')
 
 def user_login(request):
-    if request.methode == 'POST':
+    if request.method == 'POST':
         username = request.POST.get('username')
         username = request.POST.get('username')
 
@@ -30,3 +30,42 @@ def user_login(request):
 
     else:
         return render(request, 'app/login.html')
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_data = ProfileData(data=request.POST)
+
+        if user_form.is_valid() and profile_data.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_data.save(commit=False)
+            profile.save()
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+                profile.save()
+
+                registered = True
+        else:
+            print(user_form.errors, profile_data.errors)
+    
+    else:
+        user_form = UserForm
+        profile_data = ProfileData
+    
+    return render(request, 'app/register.html',
+                  {'user_form': user_form,
+                   'profile_data': profile_data,
+                   'registered': registered})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+        
