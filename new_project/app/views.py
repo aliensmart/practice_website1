686cc6f8a6e5da9d63cfbbf4e64e89d63c5e0cdc
aliensmart <kaoua17@gmail.com>
2020-05-03 01:26:23 -0400

    added user_login in view.py

--- new_project/app/views.py
+++ new_project/app/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from .forms import UserForm, ProfileData
+
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def index(request):
+    return render(request, 'app/index.html')
+
+def user_login(request):
+    if request.methode == 'POST':
+        username = request.POST.get('username')
+        username = request.POST.get('username')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            
+            else:
+                return HttpResponse("Account does not Exist or inactive")
+            
+        else:
+            return HttpResponse("Invalid Login detail")
+
+    else:
+        return render(request, 'app/login.html')
